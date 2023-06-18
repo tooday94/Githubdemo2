bbdfeab06d9daa53673d64a9da4222e68f105c71
tooday94 <masharipovbakhrom@gmail.com>
2023-06-18 23:45:13 +0500

    Initial Commit

---
+++ novvudir/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  novvudir
+//
+//  Created by Bakhrom Masharipov on 18.06.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
